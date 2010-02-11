534aff9b0bdeaa8598ed7c9dda022da7e79f86bf
Rafael Magaña <rafael.magana@crowdint.com>
2010-02-11 17:12:09 -0600

    initial commit

---
+++ database_utils.rb
@@ -0,0 +1,31 @@
+module DatabaseUtils
+  
+  def table_exists?
+    ActiveRecord::Base.connection.table_exists?(self.to_s)
+  end
+  
+  def column_exists?(column_name)
+    begin
+      ActiveRecord::Base.connection.columns(self.to_s).map(&:name).include?(column_name.to_s)
+    rescue NoMethodError #actual_columns is empty and it doesn't respond to the map method
+      false
+    end
+  end
+  
+  def named_index_exists?(index_name)
+    ActiveRecord::Base.connection.indexes(self.to_s).map(&:name).include?(index_name.to_s)
+  end
+  
+  ## I encourage you to move the following code outside this file
+  ## I put it here just to keep all the code in the same file
+  begin
+    Symbol.class_eval do
+      include DatabaseUtils
+    end
+  
+    String.class_eval do
+      include DatabaseUtils
+    end
+  end if $rails_rake_task #include module only if we are in a migration
+
+end
